a761b8baf96f1a6f2d6454e0c055af0e9b24c279
Gerhard Schraml <g.schraml@gmx.at>
2011-11-02 21:55:16 +0100

    guessing how to implement logger in CustomerRelationsManagementServiceImpl

--- src/src/at/ac/tuwien/infosys/aic11/services/CustomerRelationsManagementServiceImpl.java
+++ src/src/at/ac/tuwien/infosys/aic11/services/CustomerRelationsManagementServiceImpl.java
@@ -1,5 +1,7 @@
 package src.at.ac.tuwien.infosys.aic11.services;
 
+import java.util.logging.Logger;
+
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 
@@ -7,11 +9,20 @@ import src.at.ac.tuwien.infosys.aic11.data.Customer;
 
 @WebService(targetNamespace = "http://at.ac.tuwien.infosys.aic11.services")
 public class CustomerRelationsManagementServiceImpl implements CustomerRelationsManagementService {
-
+	
+	private Logger logger;
+	
+	public CustomerRelationsManagementServiceImpl()
+	{
+		logger = Logger.getLogger("at.ac.tuwien.infosys.aic11.services.CustomerRelationsManagementServiceImpl");
+	}
+	
 	@Override
 	@WebMethod
 	public Customer addCustomer(Customer c) {
+		logger.info("Invoking addCustomer(c = " + c + ")");
 		// TODO Auto-generated method stub
+		logger.info("Finishing addCustomer (return value = null)");
 		return null;
 	}
 
